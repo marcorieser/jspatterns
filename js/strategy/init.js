822da94ddf5f4eb991fae20b7e3f8d6dd702ad09
Marco Rieser <marco.rieser@jedesign.ch>
2015-09-22 20:58:06 +0200

    Pub/Sub Pattern

---
+++ js/strategy/init.js
@@ -0,0 +1,17 @@
+define(function (require) {
+    'use strict';
+
+    return {
+        init: function () {
+            var Strategy = require('strategy/strategy'),
+                telValidator = require('strategy/telValidator'),
+                emailValidator = require('strategy/emailValidator'),
+                validator;
+
+            validator = new Strategy();
+
+            console.log(validator.selectValidator(telValidator).validate(12345678901));
+            console.log(validator.selectValidator(emailValidator).validate('test@hello.com'));
+        }
+    };
+});
