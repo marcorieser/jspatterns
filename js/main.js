8165dc64bb13dcb66315bae57e57e457e94ef196
Marco Rieser <marco.rieser@jedesign.ch>
2015-09-25 19:07:39 +0200

    Observer Pattern

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ require(
     [
         'factory/init',
         'pubsub/init',
-        'strategy/init'
+        'strategy/init',
+        'observer/init'
     ],
-    function (factory, pubsub, strategy) {
+    function (factory, pubsub, strategy, observer) {
         'use strict';
         var examples = {
             factory: factory,
             pubsub: pubsub,
-            strategy: strategy
+            strategy: strategy,
+            observer: observer
         };
 
         window.runExamle = function (example) {
