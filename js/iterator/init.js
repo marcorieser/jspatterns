9ddab90ba36f6479024903b1d63b4ed19b5f456d
Marco Rieser <marco.rieser@jedesign.ch>
2015-09-26 13:14:40 +0200

    Iterator Pattern

--- js/iterator/init.js
+++ js/iterator/init.js
@@ -6,11 +6,16 @@ define(function (require) {
             var iterator = require('iterator/iterator'),
                 testArray = [ { something: 'yay', other: 123 }, { something: 'test', other: 456 } ],
                 myArrayIterator = iterator.build(testArray),
+                testObject = { first: 'first', second: 'second' },
+                myObjectIterator = iterator.build(testObject),
                 testString = 'teststring',
                 myStringIterator = iterator.build(testString);
 
             console.log(myArrayIterator.next());
             console.log(myArrayIterator.next());
+
+            console.log(myObjectIterator.next());
+            console.log(myObjectIterator.next());
 
             while (!myStringIterator.isDone()) {
                 console.log(myStringIterator.next());
